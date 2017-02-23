4c394188c6cd2c8cd619a138a53d2ec33152aabb
FreakJoe <sandmensch933@web.de>
2017-02-23 20:06:12 +0100

    Implemented AES encryption and decryption

--- cryptolock/security.py
+++ cryptolock/security.py
@@ -1,8 +1,40 @@
+from Crypto.Cipher import AES
+from Crypto import Random
+
 def encrypt(message, key):
-	return message + key
+	"""AES-encrypts a message using the provided key"""
+
+	if not ensure_message_validity(message):
+		return False
+
+	key = ensure_key_validity(key)
+	if not key:
+		return False
+
+	iv = Random.new().read(AES.block_size)
+	cipher = AES.new(key, AES.MODE_CFB, iv)
+	message_encrypted = iv + cipher.encrypt(message)
+
+	return message_encrypted
 
 def decrypt(message, key):
-	return message[0:len(message) - len(key)]
+	"""Decrypts an AES-encrypted message using the provided key"""
+
+	if not ensure_message_validity(message):
+		return False
+
+	key = ensure_key_validity(key)
+	if not key:
+		return False
+
+	# Retrieve the iv stored in the first AES.block_size places of the string
+	iv = message[0:AES.block_size]
+	# and the actual message stored in the remaining part of the string
+	message = message[AES.block_size:]
+	cipher = AES.new(key, AES.MODE_CFB, iv)
+	message_decrypted = cipher.decrypt(message)
+
+	return message_decrypted
 
 def ensure_key_validity(key):
 	# Ensure the key is a string
@@ -17,3 +49,14 @@ def ensure_key_validity(key):
 		return False
 
 	return key
+
+def ensure_message_validity(message):
+	# Ensure the message is a string
+	if not isinstance(message, str):
+		return False
+
+	# Ensure the message is longer than AES.block_size as it has to contain an iv of that length
+	if not len(message) > AES.block_size:
+		return False
+
+	return True
