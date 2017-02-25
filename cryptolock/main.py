5f483ad2c7310a1c649f00d481320a71408c7dd6
FreakJoe <sandmensch933@web.de>
2017-02-25 11:29:23 +0100

    Prompting again after carrying out the first command

--- cryptolock/main.py
+++ cryptolock/main.py
@@ -4,17 +4,22 @@ from cryptolock.SecureDatabase import SecureDatabase
 from cryptolock.commands import add, read
 from cryptolock.exceptions import InvalidFileException, BinaryFileException, DocumentNotFoundException, CryptoFalseKeyException, CryptoInvalidKeyException, CryptoInvalidMessageException
 
-def main():
+def main(first=True, secure_database=None):
     """Entry point for the application"""
 
-    secure_database = SecureDatabase()
+    if not secure_database:
+        secure_database = SecureDatabase()
+
+    if first:
+        print('Welcome to CryptoLock.')
+        print('For instructions please check out https://github.com/FreakJoe/cryptolockpy.')
+
     commands = {
         1: ('Lock a file into the database.', add),
         2: ('Read a file from the database.', read)
     }
 
-    print('Welcome to CryptoLock.')
-    print('For instructions please check out https://github.com/FreakJoe/cryptolockpy.\n\n')
+    print('\n\n')
     for command in sorted(commands.items(), key=lambda t: t[0]):
         output = '{} - {}'.format(command[0], command[1][0])
         print(output)
@@ -26,6 +31,7 @@ def main():
         commands[int(command)][1](secure_database)
 
     except (KeyError, ValueError, InvalidFileException, BinaryFileException, DocumentNotFoundException, CryptoFalseKeyException, CryptoInvalidKeyException, CryptoInvalidMessageException) as ex:
+        print('\n')
         if type(ex).__name__ in [KeyError.__name__, ValueError.__name__]:
             print('Your choice was invalid.')
 
@@ -49,3 +55,5 @@ def main():
 
         else:
             print('An unspecified error has occured. Please contact the developer on github.')
+
+    main(False, secure_database)
